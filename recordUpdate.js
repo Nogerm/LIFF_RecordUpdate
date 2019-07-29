2702cb6ec2c2be1ccb35546477bc495c79bb05c1
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-07-29 18:20:05 +0800

    Add loading

--- recordUpdate.js
+++ recordUpdate.js
@@ -1,7 +1,15 @@
 const hostURL = "https://script.google.com/macros/s/AKfycbyQwaNfRrnyBB4kCOvdMgUw_o6v8Z_lNUDqjNCT5Uo-dPKBvZ0/exec";
 
+var div_loading = document.getElementById("loading");
+var div_content = document.getElementById("content");
+var div_name    = document.getElementById("name");
+
 //init
 window.onload = function (e) {
+
+  div_loading.style.display = "block";
+  div_content.style.display = "none";
+
   liff.init(
     data => {
       // Now you can call LIFF API
@@ -20,8 +28,13 @@ function initializeApp(data) {
   axios.get(query_url)
   .then(function (response) {
     // Success
+    div_loading.style.display = "none";
+    div_content.style.display = "block";
+
     if(response.data.status === 200) {
       alert(JSON.stringify(response.data));
+
+      div_name.text = response.data.userName;
     } else {
       alert(response.data.message);
     }
