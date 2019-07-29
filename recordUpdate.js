5be19dd65b3246d60e1a2065f358f7adb20b12bf
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-07-29 16:18:41 +0800

    Init commit

---
+++ recordUpdate.js
@@ -0,0 +1,26 @@
+const hostURL = "https://script.google.com/macros/s/AKfycbyQwaNfRrnyBB4kCOvdMgUw_o6v8Z_lNUDqjNCT5Uo-dPKBvZ0/exec";
+
+//init
+window.onload = function (e) {
+  liff.init(function (data) {
+    initializeApp(data);
+  });
+};
+
+function initializeApp(data) {
+  //check user permission
+  const query_url = host + "?type=user_info&lineId=" + data.userId;
+  axios.get(query_url)
+  .then(function (response) {
+    // Success
+    if(response.data.status === 200) {
+
+    } else {
+      alert(response.data.message);
+    }
+  })
+  .catch(function (error) {
+    // Error
+    console.log(error);
+  })
+}
