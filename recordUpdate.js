0f92cd4626a30888540dd569dc5a8722af3137f5
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-07-30 19:11:43 +0800

    add log

--- recordUpdate.js
+++ recordUpdate.js
@@ -92,6 +92,7 @@ function send() {
     groupName: reportGroup,
     reportData: checkResult
   };
+  alert("post data: " + JSON.stringify(postData));
   axios.post(hostURL, postData)
   .then(function (response) {
     alert("server result: " + JSON.stringify(response.data));
