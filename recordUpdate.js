53dc138adda8c9d11ca4ef9d5666001a08fa941a
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-07-31 09:51:03 +0800

    fix typo

--- recordUpdate.js
+++ recordUpdate.js
@@ -92,7 +92,7 @@ function send() {
     groupName: reportGroup,
     reportData: checkResult
   };
-  alert("post data: " + JSON.stringify("Post1" + postData));
+  alert("post data 1: " + JSON.stringify(postData));
   axios.post(hostURL, JSON.stringify(postData))
   .then(function (response) {
     alert("server result: " + JSON.stringify(response.data));
