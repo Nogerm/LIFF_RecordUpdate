3eabf6f743b5203548ee636b4b671dbc71e068aa
Andy Wu <andywuchin@gmail.com>
2019-09-21 14:32:21 +0800

    Add debug log

--- recordUpdate.js
+++ recordUpdate.js
@@ -171,7 +171,7 @@ function send() {
       time: reportTimeStr,
       atendee: JSON.stringify(reportAtendee)
     };
-    alert("post data: " + postData);
+    alert("post data: " + JSON.stringify(postData));
 
     $.ajax({
       url: hostURL,
