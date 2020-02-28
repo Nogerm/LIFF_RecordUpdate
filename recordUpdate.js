957f976a0726ded19cad526397de001584e61daf
Andy CW Wu <andy_cw_wu@wistron.com>
2020-02-28 13:51:53 +0800

    Update

--- recordUpdate.js
+++ recordUpdate.js
@@ -1,5 +1,5 @@
 const hostURL = "https://script.google.com/macros/s/AKfycbyQwaNfRrnyBB4kCOvdMgUw_o6v8Z_lNUDqjNCT5Uo-dPKBvZ0/exec";
-const liffID = "1602321395-7wQx5L66";
+const liffID = "1653896800-lZBLPy1z";
 
 var allMembers = [];
 var allEvents = [];
@@ -10,8 +10,7 @@ var selectedEventIndex = undefined;
 
 //init
 window.onload = function (e) {
-  liff.init(
-    {
+  liff.init({
       liffId: liffID
     },
     data => {
@@ -58,46 +57,46 @@ function initializeApp(profile) {
   showSegmentLoading();
   const query_url = hostURL + "?type=report_basic&lineId=" + profile.userId;
   axios.get(query_url)
-  .then(response => {
-    // Success
-    hideSegmentLoading();
+    .then(response => {
+      // Success
+      hideSegmentLoading();
 
-    if(response.data.status === 200) {
-      //update time container
-      allEvents = response.data.eventTime;
-      updateTimeContainer(allEvents.reverse());
+      if (response.data.status === 200) {
+        //update time container
+        allEvents = response.data.eventTime;
+        updateTimeContainer(allEvents.reverse());
 
-      //update member container
-      allMembers = response.data.groupMembers;
-      updateMemberContainer(allMembers);
+        //update member container
+        allMembers = response.data.groupMembers;
+        updateMemberContainer(allMembers);
 
-    } else if(response.data.status === 512) {
-      swal.fire({
-        title: '沒有權限',
-        text: '請先到設定頁面，申請成為回報人員',
-        type: 'error',
-        onClose: () => {
-          liff.closeWindow();
-        }
-      });
-    } else {
+      } else if (response.data.status === 512) {
+        swal.fire({
+          title: '沒有權限',
+          text: '請先到設定頁面，申請成為回報人員',
+          type: 'error',
+          onClose: () => {
+            liff.closeWindow();
+          }
+        });
+      } else {
+        swal.fire({
+          title: '錯誤',
+          text: response.data.message,
+          type: 'error'
+        });
+      }
+    })
+    .catch(error => {
+      // Error
+      hideSegmentLoading();
+      console.log(error);
       swal.fire({
         title: '錯誤',
-        text: response.data.message,
+        text: error,
         type: 'error'
       });
-    }
-  })
-  .catch(error => {
-    // Error
-    hideSegmentLoading();
-    console.log(error);
-    swal.fire({
-      title: '錯誤',
-      text: error,
-      type: 'error'
     });
-  });
 }
 
 function getEventId(event) {
@@ -108,18 +107,18 @@ function updateTimeContainer(events) {
   const timeContainer = document.getElementById("time-container");
 
   selectedEventIndex = 0;
-  if(selectedEventId === undefined) selectedEventId = getEventId(events[selectedEventIndex]);
+  if (selectedEventId === undefined) selectedEventId = getEventId(events[selectedEventIndex]);
   reportAtendee = JSON.parse(JSON.stringify(allEvents[selectedEventIndex].attendee));
-  
+
   events.forEach((event, index) => {
     //create time button
     let btn = document.createElement("button");
-    btn.innerHTML = event.timestring.substr(4,2) + '/' + event.timestring.substr(6,2) + '<br>' + event.type;
+    btn.innerHTML = event.timestring.substr(4, 2) + '/' + event.timestring.substr(6, 2) + '<br>' + event.type;
     btn.setAttribute("class", (selectedEventId === getEventId(event)) ? "ui primary button" : "ui primary basic button");
-	  btn.setAttribute("id", getEventId(event));
+    btn.setAttribute("id", getEventId(event));
     btn.setAttribute("value", index);
     btn.style.marginBottom = "8px";
-    btn.onclick = function(element) {
+    btn.onclick = function (element) {
       selectedEventId = element.target.id;
       selectedEventIndex = element.target.value;
       reportAtendee = JSON.parse(JSON.stringify(allEvents[selectedEventIndex].attendee));
@@ -141,7 +140,9 @@ function updateTimeContainer(events) {
 
 function updateMemberContainer(memberGroups) {
   const memberContainer = document.getElementById("member-container");
-  while (memberContainer.firstChild) { memberContainer.removeChild(memberContainer.firstChild)}
+  while (memberContainer.firstChild) {
+    memberContainer.removeChild(memberContainer.firstChild)
+  }
 
   memberGroups.forEach((memberGroup, index) => {
 
@@ -162,14 +163,14 @@ function updateMemberContainer(memberGroups) {
       btn.setAttribute("id", memberName);
       btn.setAttribute("value", memberName);
       btn.style.marginBottom = "8px";
-      btn.onclick = function(element) {
-        if(element.target.className === "ui primary button") {
+      btn.onclick = function (element) {
+        if (element.target.className === "ui primary button") {
           //currently selected, to deselect
           const deleteIdx = reportAtendee.indexOf(element.target.value);
-          if(deleteIdx > -1) reportAtendee.splice(deleteIdx, 1);
-        } else if(element.target.className === "ui primary basic button"){
+          if (deleteIdx > -1) reportAtendee.splice(deleteIdx, 1);
+        } else if (element.target.className === "ui primary basic button") {
           //currently deselected, to select
-          if(reportAtendee.indexOf(element.target.value) === -1) reportAtendee.push(element.target.value);
+          if (reportAtendee.indexOf(element.target.value) === -1) reportAtendee.push(element.target.value);
         }
         console.log(JSON.stringify(reportAtendee));
 
@@ -192,65 +193,65 @@ function arrayify(collection) {
 }
 
 function send() {
-  
+
   console.log("reportAtendee:" + JSON.stringify(reportAtendee));
 
   liff.getProfile()
-  .then(profile => {
-    //add loading to button
-    const btn = document.getElementById("submitBtn");
-    btn.className = "fluid ui loading button";
+    .then(profile => {
+      //add loading to button
+      const btn = document.getElementById("submitBtn");
+      btn.className = "fluid ui loading button";
 
-    const postData = {
-      lineId: profile.userId,
-      type: 'report_attendee',
-      time: allEvents[selectedEventIndex].timestamp,
-      reportType: allEvents[selectedEventIndex].type,
-      attendee: JSON.stringify(reportAtendee)
-    };
+      const postData = {
+        lineId: profile.userId,
+        type: 'report_attendee',
+        time: allEvents[selectedEventIndex].timestamp,
+        reportType: allEvents[selectedEventIndex].type,
+        attendee: JSON.stringify(reportAtendee)
+      };
 
-    $.ajax({
-      url: hostURL,
-      type: "POST",
-      datatype: "json",
-      data: postData,
-      success: function (res, status) {
-        //alert("server result: " + JSON.stringify(res) + "\nstatus: " + status);
-        btn.className = "fluid ui button";
+      $.ajax({
+        url: hostURL,
+        type: "POST",
+        datatype: "json",
+        data: postData,
+        success: function (res, status) {
+          //alert("server result: " + JSON.stringify(res) + "\nstatus: " + status);
+          btn.className = "fluid ui button";
 
-        if(res.status === 500) {
+          if (res.status === 500) {
+            swal.fire({
+              title: '錯誤',
+              text: res.message,
+              type: 'error'
+            });
+          } else if (res.status === 200) {
+            swal.fire({
+              title: '回報成功',
+              text: '點擊確定關閉視窗',
+              type: 'success',
+              onClose: () => {
+                //liff.closeWindow();
+              }
+            });
+          }
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+          btn.className = "fluid ui button";
           swal.fire({
             title: '錯誤',
-            text: res.message,
+            text: "post error: " + xhr.responseText + "\najaxOptions: " + ajaxOptions + "\nthrownError: " + thrownError,
             type: 'error'
           });
-        } else if(res.status === 200) {
-          swal.fire({
-            title: '回報成功',
-            text: '點擊確定關閉視窗',
-            type: 'success',
-            onClose: () => {
-              //liff.closeWindow();
-            }
-          });
         }
-      },
-      error: function(xhr, ajaxOptions, thrownError) {
-        btn.className = "fluid ui button";
-        swal.fire({
-          title: '錯誤',
-          text: "post error: " + xhr.responseText + "\najaxOptions: " + ajaxOptions + "\nthrownError: " + thrownError,
-          type: 'error'
-        });
-      }
+      });
+    })
+    .catch((err) => {
+      console.log('error', err);
+      swal.fire({
+        title: '錯誤',
+        text: err,
+        type: 'error'
+      });
     });
-  })
-  .catch((err) => {
-    console.log('error', err);
-    swal.fire({
-      title: '錯誤',
-      text: err,
-      type: 'error'
-    });
-  });
 }
